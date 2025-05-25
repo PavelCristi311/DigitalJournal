b86fb5557a380ea9ae2ab3fb426dc6748e99e5d7
Cristian Terheș-Pavel <pavelcristi205@yahoo.com>
2025-05-25 20:01:28 +0300

    Introduced the resources for the app logo and modified the main function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
 
-int main() {
-    return 0;
+#include <QApplication>
+#include "RepositoryLayer/JSONRepo.h"
+#include "RepositoryLayer/CSVRepo.h"
+#include "ControllerLayer/Controller.h"
+#include "UILayer/MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QApplication::setWindowIcon(QIcon(":images/UILayer/logo.png"));
+    JSONRepo repo("journal.json");
+    repo.load();
+    CSVRepo repo2("journal.csv");
+    repo2.load();
+    Controller controller(&repo2);
+    MainWindow window(&controller);
+    window.show();
+    return QApplication::exec();
 }
